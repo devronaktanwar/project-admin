4f1acb2791913227bd0ad1a0782b4c2854883896
ronaktanwar <ronak@orufy.com>
2025-06-08 13:51:47 +0530

    fix : type error

--- components/appointments/AddAppointment.tsx
+++ components/appointments/AddAppointment.tsx
@@ -8,13 +8,6 @@ import { CalendarIcon, Clock } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
